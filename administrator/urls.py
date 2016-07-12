34b84eaa98ec808d73ab86eb1f530fe3093f47a3
SergioPolanco <polancosergio8@gmail.com>
2016-07-12 13:55:30 +0200

    Armando admin

---
+++ administrator/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.home_view, name='administrator.index'),
+    #url(r'^login/$', views.login_view, name='administrator.login'),
+    #url(r'^logout/$', views.logout_view, name='administrator.logout'),
+]
